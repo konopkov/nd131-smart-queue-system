451f32bca3e7d783b81a57a555a82327a1ab4753
Andrey Konopkov <konopkov@KNPKV-MB-PRO.local>
2020-07-15 15:55:38 +0300

    Assignment completed

---
+++ person_detect.py
@@ -0,0 +1,208 @@
+
+import numpy as np
+import time
+from openvino.inference_engine import IECore
+import os
+import cv2
+import argparse
+import sys
+
+
+class Queue:
+    '''
+    Class for dealing with queues
+    '''
+    def __init__(self):
+        self.queues=[]
+
+    def add_queue(self, points):
+        self.queues.append(points)
+
+    def get_queues(self, image):
+        for q in self.queues:
+            x_min, y_min, x_max, y_max=q
+            frame=image[y_min:y_max, x_min:x_max]
+            yield frame
+    
+    def check_coords(self, coords):
+        d={k+1:0 for k in range(len(self.queues))}
+        for coord in coords:
+            for i, q in enumerate(self.queues):
+                if coord[0]>q[0] and coord[2]<q[2]:
+                    d[i+1]+=1
+        return d
+
+
+class PersonDetect:
+    '''
+    Class for the Person Detection Model.
+    '''
+
+    def __init__(self, model_name, device, threshold=0.60):
+        self.model_weights=model_name+'.bin'
+        self.model_structure=model_name+'.xml'
+        self.device=device
+        self.threshold=threshold
+
+        try:
+            self.core = IECore()
+            self.model = self.core.read_network(model=self.model_structure, weights=self.model_weights)
+        except Exception as e:
+            raise ValueError("Could not Initialise the network. Have you enterred the correct model path?")
+
+        self.input_name=next(iter(self.model.inputs))
+        self.input_shape=self.model.inputs[self.input_name].shape
+        self.output_name=next(iter(self.model.outputs))
+        self.output_shape=self.model.outputs[self.output_name].shape
+
+    def load_model(self):
+        '''
+        Loading model into network
+        '''
+        self.net = self.core.load_network(network=self.model, device_name=self.device, num_requests=1)
+        
+    def predict(self, image):
+        '''
+        Prediction
+        '''
+        self.width = int(image.shape[1]) 
+        self.height = int(image.shape[0])
+        frame = self.preprocess_input(image)
+        input_dict = {self.input_name: frame}
+        outputs = self.net.infer(input_dict)
+        coords = self.preprocess_outputs(outputs[self.output_name])
+        self.draw_outputs(coords, image)
+        
+        return coords, image
+    
+    def draw_outputs(self, coords, image):
+        '''
+        Drawing boxes on the frame
+        '''
+        
+        for obj in coords:
+            x_1 = int(obj[0])
+            y_1 = int(obj[1])
+            x_2 = int(obj[2])
+            y_2 = int(obj[3])
+            cv2.rectangle(image, (x_1, y_1), (x_2, y_2), (0, 0, 255), 1)
+
+        return image
+
+    def preprocess_outputs(self, outputs):
+        '''
+        Preprocessng outputs
+        '''
+        coords = []
+        for out in outputs[0][0]:
+            # out[2] -> probability
+            if (out[2] >= self.threshold):
+                coords.append((
+                    # x_1
+                    out[3] * self.width,
+                    # y_1
+                    out[4] * self.height,
+                    # x_2
+                    out[5] * self.width,
+                    # 4_2
+                    out[6] * self.height
+                    ))
+        return coords
+
+    def preprocess_input(self, image):
+        '''
+        Preprocessing input frame
+        '''
+        image = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
+        image = image.transpose((2, 0, 1))
+        image = image.reshape(1, *image.shape)
+        return image
+
+def main(args):
+    model=args.model
+    device=args.device
+    video_file=args.video
+    max_people=args.max_people
+    threshold=args.threshold
+    output_path=args.output_path
+
+    start_model_load_time=time.time()
+    pd= PersonDetect(model, device, threshold)
+    pd.load_model()
+    total_model_load_time = time.time() - start_model_load_time
+
+    queue=Queue()
+    
+    try:
+        queue_param=np.load(args.queue_param)
+        for q in queue_param:
+            queue.add_queue(q)
+    except:
+        print("error loading queue param file")
+
+    try:
+        cap=cv2.VideoCapture(video_file)
+    except FileNotFoundError:
+        print("Cannot locate video file: "+ video_file)
+    except Exception as e:
+        print("Something else went wrong with the video file: ", e)
+    
+    initial_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    out_video = cv2.VideoWriter(os.path.join(output_path, 'output_video.mp4'), cv2.VideoWriter_fourcc(*'avc1'), fps, (initial_w, initial_h), True)
+    
+    counter=0
+    start_inference_time=time.time()
+
+    try:
+        while cap.isOpened():
+            ret, frame=cap.read()
+            if not ret:
+                break
+            counter+=1
+            
+            coords, image= pd.predict(frame)
+            num_people= queue.check_coords(coords)
+            print(f"Total People in frame = {len(coords)}")
+            print(f"Number of people in queue = {num_people}")
+            out_text=""
+            y_pixel=25
+            
+            for k, v in num_people.items():
+                out_text += f"No. of People in Queue {k} is {v} "
+                if v >= int(max_people):
+                    out_text += f" Queue full; Please move to next Queue "
+                cv2.putText(image, out_text, (15, y_pixel), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                out_text=""
+                y_pixel+=40
+            out_video.write(image)
+            
+        total_time=time.time()-start_inference_time
+        total_inference_time=round(total_time, 1)
+        fps=counter/total_inference_time
+
+        with open(os.path.join(output_path, 'stats.txt'), 'w') as f:
+            f.write(str(total_inference_time)+'\n')
+            f.write(str(fps)+'\n')
+            f.write(str(total_model_load_time)+'\n')
+
+        cap.release()
+        cv2.destroyAllWindows()
+    except Exception as e:
+        print("Could not run Inference: ", e)
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--model', required=True)
+    parser.add_argument('--device', default='CPU')
+    parser.add_argument('--video', default=None)
+    parser.add_argument('--queue_param', default=None)
+    parser.add_argument('--output_path', default='/results')
+    parser.add_argument('--max_people', default=2)
+    parser.add_argument('--threshold', default=0.60)
+    
+    args=parser.parse_args()
+
+    main(args)
